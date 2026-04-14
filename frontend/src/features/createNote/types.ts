b0cc061b97828d17e5da69c40473b3ae0eff289b
mohanishp9 <mpingale31@gmail.com>
2026-04-14 22:30:35 +0530

    Create Note RTK

---
+++ frontend/src/features/createNote/types.ts
@@ -0,0 +1,38 @@
+import { Note } from "@/features/publicNote/types";
+
+export interface CreateNoteParams {
+    showUsername: boolean;
+    subject: string;
+    content: string;
+    category: "😂" | "😡" | "😳" | "😭";
+}
+
+export interface CreateNoteResponse {
+    success: boolean;
+    data: Note;
+}
+
+export interface CreateNoteState {
+    loading: boolean;
+    error: string | null;
+    success: boolean;
+}
+
+
+// {
+//     "success": true,
+//     "data": {
+//         "user": "69cd564b2d219d5a8dcf3aa4",
+//         "showUsername": false,
+//         "subject": "Bus Seat Bag",
+//         "content": "Someone put their bag on the empty seat beside them. I stood right next to it staring until they moved it with an eye roll 😳",
+//         "categoryEmoji": "😳",
+//         "likes": 0,
+//         "reactionsCount": {},
+//         "commentsCount": 0,
+//         "_id": "69de4f2767526f64dffc9187",
+//         "createdAt": "2026-04-14T14:28:55.859Z",
+//         "updatedAt": "2026-04-14T14:28:55.859Z",
+//         "__v": 0
+//     }
+// }
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,12 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@/features/auth/authSlice";
 import publicNoteReducer from "@/features/publicNote/publicNoteSlice";
 import commentsReducer from "@/features/comments/commentsSlice";
+import createNoteReducer from "@/features/createNote/createNoteSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         publicNote: publicNoteReducer,
         comments: commentsReducer,
+        createNote: createNoteReducer,
+
     },
 });
 
